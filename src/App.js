8c83305c63965a02ed881ca35db585b27f90053e
aliabdulazeez <aliabdulazeez01@gmail.com>
2021-03-16 11:07:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { ParentBox, ChildBox } from "./Vanilla";
+
+const StyledCanvas = styled.div`
+  background: #e76f51;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWrapper = styled.div`
+  display: grid;
+  width: 1000px;
+  height: 1000px;
+  background-color: #2a9d8f;
+  border-radius: 5px;
+  margin: 0 auto;
+  padding: 10px;
+  justify-content: center;
+  align-content: center;
+  justify-items: center;
+  align-items: center;
+  border: 6px solid #d3c6a6;
+  grid-template-columns: 200px 200px 200px 200px 200px;
+  grid-template-rows: 200px 200px 200px 200px 200px;
+`;
+
+const StyledEmpty = styled.div`
+  height: 150px;
+  width: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+`;
+
+const StyledFill = styled.div`
+  background-color: #264653;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #e0e0e0;
+  margin: 5px;
+  height: 130px;
+  width: 130px;
+  cursor: pointer;
+  user-select: none;
+`;
 
 function App() {
+  const [hasChild, setHasChild] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledCanvas>
+      <StyledWrapper>
+        <ParentBox>
+          {/* Take from this and ... */}
+          <ChildBox key="1" id="a">
+            A
+          </ChildBox>
+        </ParentBox>
+
+        <ParentBox>{/* Insert Here for e.g. */}</ParentBox>
+
+        <ParentBox>{/* Or Here ... */}</ParentBox>
+      </StyledWrapper>
+    </StyledCanvas>
   );
 }
 
---
+++ src/Vanilla.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+//Styles of the ParentBox
+
+const StyledEmpty = styled.div`
+  height: 150px;
+  width: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  border: ${({ hovered }) => (hovered ? "4px solid #fffcfc" : "none")};
+`;
+
+export function ParentBox({ children, id }) {
+  const [over, setOver] = useState(false);
+  console.log("Child: " + children, "Id: " + id);
+
+  function dragOver(e) {
+    e.preventDefault();
+    console.log("drag over");
+  }
+  function dragEnter(e) {
+    e.preventDefault();
+    setOver(true);
+    console.log("drag enter");
+  }
+  function dragLeave() {
+    setOver(false);
+    console.log("drag leave");
+  }
+  function dragDrop(e) {
+    console.log("drag drop");
+    setOver(false);
+    console.log(e);
+  }
+
+  return (
+    <StyledEmpty
+      hovered={over}
+      onDragOver={dragOver}
+      onDragEnter={dragEnter}
+      onDragLeave={dragLeave}
+      onDrop={dragDrop}
+    >
+      {children}
+    </StyledEmpty>
+  );
+}
+
+const fill = {
+  backgroundColor: "#264653",
+  position: "absolute",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "2rem",
+  color: "#fff",
+  borderRadius: "10px",
+  border: "2px solid #e0e0e0",
+  margin: "5px",
+  height: "130px",
+  width: "130px",
+  cursor: "pointer",
+  userSelect: "none",
+};
+
+const invisible = {
+  display: "none",
+};
+
+export function ChildBox({ children, id }) {
+  const [start, setStart] = useState(false);
+
+  function dragStart() {
+    setTimeout(() => setStart(true), 0);
+
+    console.log("drag start");
+  }
+
+  function dragEnd() {
+    console.log("drag end");
+    setStart(false);
+  }
+
+  return (
+    <div
+      style={start ? invisible : fill}
+      id={id}
+      draggable="true"
+      onDragStart={dragStart}
+      onDragEnd={dragEnd}
+    >
+      {children}
+    </div>
+  );
+}
